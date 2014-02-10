6ed27ec457d4791d2a97f17a676ab4751ca9a8b9
Addy Bhardwaj <contact@knaptus.com>
2014-02-10 23:55:07 +0000

    test case scenarios

--- npcomplete/src/test/java/com/knaptus/coding/handson/npcomplete/optaplanner/matcher/MatchingDataGenerator.java
+++ npcomplete/src/test/java/com/knaptus/coding/handson/npcomplete/optaplanner/matcher/MatchingDataGenerator.java
@@ -8,4 +8,17 @@ package com.knaptus.coding.handson.npcomplete.optaplanner.matcher;
 public class MatchingDataGenerator {
 
 
+    //positive and realistic scenarios
+
+    //10, 100, 1000 txns
+    //price variation within 5% to 10%, 50%, 1000%
+    //quantity between 10 to 20
+    //select random set of 20%, 50%, 70%, 100% trades
+
+    //unrealistic scenarios
+
+    //Add spike to the data set i.e. 2% of spike
+    //select with and without spike
+
+    //impossible scenarios
 }
